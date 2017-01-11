097ea5b4aabd29eb3cdfc40d812d6af53c1eccdd
Danil <homie.dont.kill.my.vibe@gmail.com>
2017-01-11 21:51:07 +0300

    Heads will roll

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  $('sl_one').slick();
+});
+
+                <div class="recom">Уборка после ремонта</div>
+                <div class="recomText">Lorem ipsum dolor sit amet, 
+consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+officia deserunt mollit anim id est laborum. </div>
+                <div>Полина Рыбалкина <span class="data">03.11.2016</span></div>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="block">
+                <div class="recom">Уборка после ремонта</div>
+                <div>
+                <img src="images/mail.jpg" alt="">
+                </div>
+                <div>Ксения Чернова <span class="data">03.11.2016</span></div>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="block">
+                <div class="recom">Уборка после ремонта</div>
+                <div class="recomText">Lorem ipsum dolor sit amet, 
+consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+officia deserunt mollit anim id est laborum. </div>
+                <div>Полина Рыбалкина <span class="data">03.11.2016</span></div>
+
+ <div class="col-sm-1"><a href="#" class="greenDate active">2016</a></div>
