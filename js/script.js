b93767ebc788f28e2ddbb8653679bf951be5d48b
Danil <homie.dont.kill.my.vibe@gmail.com>
2017-01-19 20:49:00 +0300

    Dont Kill My Vibe

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-$('.btnNav').click(function(){
+$('#btnNav').click(function(){
     $('.navigation').fadeToggle(500);
-    $($this).toggleClass("active");
+    $(this).toggleClass("active");
 });
 
     
@@ -49,26 +49,117 @@ $('.btnNav').click(function(){
     
     //SLIDER
     $(function() {
-    $('.jcarousel').jcarousel({
-        // Configuration goes here
-    });
-    });
-    $('.prevSL').click(function() {
-    $('.jcarousel').jcarousel('scroll', '-=1');
-    });
+        var carousel = $('.jcarousel').jcarousel({
+            // Configuration goes here
 
-    $('.nextSL').click(function() {
-    $('.jcarousel').jcarousel('scroll', '+=1');
-        
-    $('.jcarousel-pagination').jcarouselPagination({
-        item: function(page) {
-            return '<a href="#' + page + '">' + page + '</a>';
-        },
+            list: '.jcarousel-list',
+            items: '.jcarousel-item',
+            wrap: 'circular'
+        }).jcarouselAutoscroll({
+            interval: 3000,
+            target: '+=1',
+            autostart: true
+        });
+        $('.prevSL').jcarouselControl({
+            target: '-=1',
+            carousel: carousel
+        });
+        $('.nextSL').jcarouselControl({
+            target: '+=1',
+            carousel: carousel
+        });
+        var target = $('.jcarousel').jcarousel('target');
+        $('.jcarousel-pagination').on('jcarouselpagination:active', 'a', function () {
+            $(this).addClass('active');
+        })
+            .on('jcarouselpagination:inactive', 'a', function () {
+                $(this).removeClass('active');
+            }).jcarouselPagination({
+            'perPage': 1,
+            'item': function (page, carouselItems) {
+
+                return '<a class="carouselPagination" href="#' + page + '"></a>';
+            }
+        });
+        /*WORKERS*/
+        $('.workers-carousel')
+            .on('jcarousel:create jcarousel:reload', function() {
+                var element = $(this),
+                    width = element.innerWidth();
+
+                if (width > 900) {
+                    width = width / 4;
+                } else if (width > 600) {
+                    width = width / 2;
+                }
+
+                element.jcarousel('items').css('width', width + 'px');
+            })
+        var wcarousel = $('.workers-carousel').jcarousel({
+            list: '.workers-list',
+            items: '.workers-item',
+            animation: {
+                duration: 800,
+                easing:   'linear',
+                complete: function() {
+                }},
+            wrap: 'circular'
+        }).jcarouselAutoscroll({
+            interval: 3000,
+            target: '+=1',
+            autostart: true
+        });
+        $('.workerPrevSL').jcarouselControl({
+            target: '-=1',
+            carousel: wcarousel
+        });
+        $('.workerNextSL').jcarouselControl({
+            target: '+=1',
+            carousel: wcarousel
+        });
+        $('.work .workers-carousel')
+            .on('jcarousel:create jcarousel:reload', function() {
+                var element = $(this),
+                    width = element.innerWidth();
+
+                if (width > 900) {
+                    width = width / 3.2;
+                } else if (width > 600) {
+                    width = width / 2;
+                }
+
+                element.jcarousel('items').css('width', width + 'px');
+            })
+        var wcarousel = $('.work .workers-carousel').jcarousel({
+            list: '.workers-list',
+            items: '.workers-item',
+            animation: {
+                duration: 800,
+                easing:   'linear',
+                complete: function() {
+                }},
+            wrap: 'circular'
+        }).jcarouselAutoscroll({
+            interval: 3000,
+            target: '+=1',
+            autostart: true
+        });
+        $('.work .workerPrevSL').jcarouselControl({
+            target: '-=1',
+            carousel: wcarousel
+        });
+        $('.work .workerNextSL').jcarouselControl({
+            target: '+=1',
+            carousel: wcarousel
+        });
     });
-        
+    //workers slider
+
 });
+
+
     
-});
+
 
 
 
