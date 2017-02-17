8a23455a19da3083ab165863dd0058da1443c8a7
Danil <homie.dont.kill.my.vibe@gmail.com>
2017-02-17 22:01:56 +0300

    add shop(2)

--- project/furniture/js/script.js
+++ project/furniture/js/script.js
@@ -1,10 +1,10 @@
  $(document).ready(function () {
+     
+     
      $(".show").click(function(){
-         var pred = $(this).parent();
-         console.log(pred)
-         var img = pred.siblings("img")
-         console.log(img)
-        var lightbox = lity( img.attr("src"))
+        var lightbox = lity( $(this).parent().siblings("img").attr("src"))
         $(document).on('click', '[data-lightbox]', lity);
      });
+     
+     
  });
